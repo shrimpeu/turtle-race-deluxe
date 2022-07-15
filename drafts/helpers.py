6ddbdb44843d0a9284face82f91b9ed48946d58d
Darylle Villanueva <ekflfdv29@gmail.com>
2022-07-15 08:51:51 +0800

    Add update balance function

--- drafts/helpers.py
+++ drafts/helpers.py
@@ -33,3 +33,14 @@ def get_existing_email(input_email: str) -> str:
         existing_email = existing_email.encode("utf-8")
         if bcrypt.checkpw(input_email, existing_email):
             return existing_email.decode("utf-8").replace("'", '"')
+
+def update_balance(email: str, new_balance: int) -> None:
+    print(email)
+    print(new_balance)
+    with open("user_data.json", "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    data[email]["balance"] = new_balance
+
+    with open("user_data.json", "w") as jsonFile:
+        json.dump(data, jsonFile, indent=4, separators=(",", ": "), sort_keys=True)
