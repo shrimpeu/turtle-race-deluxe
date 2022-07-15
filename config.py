0e00bfcec262cfe4c7947e3fb5550973fbee0ce3
Darylle Villanueva <ekflfdv29@gmail.com>
2022-07-15 23:17:34 +0800

    change font

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ TURTLE RACE
 """
 
 # Styles
-LARGE_FONT = ("Verdana", 20)
-NORMAL_FONT = ("Verdana", 12)
+LARGE_FONT = ("Poppins", 20)
+NORMAL_FONT = ("Poppins", 12)
 FG_1 = "#ebdbb2"
 BG_1 = "#282828"
 BG_2 = "#1d2021"
