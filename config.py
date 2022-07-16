8dcd4b44edd79b6e7ee89d3bb463e2245164e3d9
Darylle Villanueva <ekflfdv29@gmail.com>
2022-07-16 20:22:44 +0800

    add font, fg, bg, config

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
-TITLE = """Welcome to
-TURTLE RACE
-"""
+TITLE = """TURTLE   RACE
+(DELUXE)"""
 
 APP_LOGO = "assets/app_logo.ico"
+GRADIENT_BG_PATH = "assets/mainpage_bg.png"
 
 # Styles
-LARGE_FONT = ("Poppins", 20)
-NORMAL_FONT = ("Poppins", 12)
-FG_1 = "#ebdbb2"
-BG_1 = "#282828"
-BG_2 = "#1d2021"
+TITLE_FONT = ("ArcadeClassic", 44, "bold")
+SUBTITLE_FONT = ("ArcadeClassic", 34, "bold")
+LARGE_FONT = ("ArcadeClassic", 24)
+NORMAL_FONT = ("ArcadeClassic", 16)
+TITLE_FG = "#335961"
+BUTTON_BG = "#b5ea24"
+BUTTON_FG = TITLE_FG
+ENTRY_BG = "#ffffff"
+ENTRY_FG = "#000a00"
+BORDER_FILL = "#d1d1d1"
+FG_1 = "#000000"
+BG_1 = "#cedeba"
+BG_2 = "#4b6043"
